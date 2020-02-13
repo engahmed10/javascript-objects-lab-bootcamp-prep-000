cc4f5a54f98bca1fc72e6cb34d4ea7ed5b39c1e4
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 11:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key)
 function  destructivelyDeleteFromObjectByKey(object, key)
 {
   
-   object[key]="undefined"
+   object[key] =parseInt("undefined")
   return (Object.assign ({},object[key]));
   
 }
